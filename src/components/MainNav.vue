8d764f2461e43d07698cf40cb1692cbd3a4c9a6f
Ghyasuddin Shahab <49924859+GhyasShahab@users.noreply.github.com>
2024-02-20 08:25:40 +0430

    new changes in view

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,9 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+        <a v-bind:href="url" class="flex items-center h-full text-xl">{{
+          company
+        }}</a>
       </div>
     </div>
   </header>
@@ -15,6 +17,7 @@ export default {
   data() {
     return {
       company: "Bobo Careers",
+      url: "www.google.com",
     };
   },
 };
