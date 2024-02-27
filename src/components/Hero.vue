1550f32438239dcfdd663feb0e10875f2469244c
Ghyasuddin Shahab <49924859+GhyasShahab@users.noreply.github.com>
2024-02-27 10:23:40 +0430

    Changed the background color of the hero section from yellow to white. Removed the blue borders around the columns in the grid

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,20 +1,14 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
+    <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-start-1 col-span-1 border border-blue-800">
-          1 column
-        </div>
-        <div class="col-start-2 col-span-5 border border-blue-800">
+        <div class="col-start-1 col-span-1 border"></div>
+        <div class="col-start-2 col-span-5 border">
           <headline />
           <job-search-form />
         </div>
-        <div class="col-start-7 col-span-5 border border-blue-800">
-          5 column
-        </div>
-        <div class="col-start-12 col-span-1 border border-blue-800">
-          1 column
-        </div>
+        <div class="col-start-7 col-span-5 border"></div>
+        <div class="col-start-12 col-span-1 border"></div>
       </div>
     </section>
   </main>
